364aa165e2b05b4d6e635bf827ba8e0234a197de
Jenny Zeng <jennyzengzzh@gmail.com>
2017-10-16 23:54:39 -0700

    add scenes, sprites, some scripts
    1. add Level1 example scene  (including player movement and simple
    ground)
    2. make sprites always fit the screen size
    3. add simple sprites as example
    4. add  layers: platforms, players, enemies
    5. add health abstract class  as base

--- Assets/Scripts/Avatar/PlayerMovement.cs
+++ Assets/Scripts/Avatar/PlayerMovement.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+	public float maxSpeed = 10f;
+	public bool facingRight = true;
+	Rigidbody2D rigid;
 
 	// Use this for initialization
 	void Start () {
-		
+		rigid = GetComponent<Rigidbody2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+		rigid.velocity = new Vector2(move * maxSpeed, rigid.velocity.y);
+
+		if ((move > 0 && !facingRight) || (move < 0 && facingRight)) Flip();
+	}
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
