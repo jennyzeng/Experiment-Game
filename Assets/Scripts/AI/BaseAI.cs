78246c14d41360f3868d8010a54abc6d7eefafcc
jennyzeng <jennyzengzzh@gmail.com>
2017-10-30 00:44:09 -0700

    update ai base class with idling. Example is in Frog ai

--- Assets/Scripts/AI/BaseAI.cs
+++ Assets/Scripts/AI/BaseAI.cs
@@ -5,28 +5,40 @@ using UnityEngine;
 public abstract class BaseAI : MonoBehaviour
 {
     public bool facingRight = true;
-    public float monitor_range = 10f;
+    public float monitorRange = 10f;
+
     public Transform[] idleRoute;
+    [HideInInspector]
+    public bool isIdling;
     protected Rigidbody2D rigid;
     protected Animator anim;
     protected GameObject player;
+    protected int nextIdlePoint;
+    protected int next;
 
     protected virtual void Start()
     {
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
+        if (idleRoute.Length == 0)
+        {
+            Debug.LogError("please add idle route for the monster " + gameObject.name);
+            Destroy(gameObject);
+        }
+        nextIdlePoint = 1;
+        next = 1;
+        isIdling = true;
+        transform.position = idleRoute[0].position;
+
     }
 
-    /// <summary>
-    /// Update is called every frame, if the MonoBehaviour is enabled.
-    /// </summary>
-    protected virtual void Update()
+    protected virtual void FixedUpdate()
     {
         if (player == null)
             player = GameManager.Instance.GetManager<GameObjectManager>().player;
         if (player == null)
             return;
-        if (Mathf.Abs(player.transform.position.x - transform.position.x) > monitor_range)
+        if (Vector2.Distance(player.transform.position, transform.position) > monitorRange)
         {
             Idle();
         }
@@ -35,7 +47,59 @@ public abstract class BaseAI : MonoBehaviour
             FoundPlayerBehavior();
         }
     }
-    protected abstract void Idle();
-    protected abstract void FoundPlayerBehavior();
+    protected virtual void Idle()
+    {
+        if (!isIdling)
+        {// start idling
+            TransFromFollowPlayerToIdle();
+            isIdling = true;
+            return;
+        }
+        if (NeedCommand())
+        {
+            GoToNextPoint(idleRoute[nextIdlePoint].position);
+            if (ShouldUpdateNextPoint())
+            {
+                if (nextIdlePoint >= idleRoute.Length-1 || nextIdlePoint == 0)
+                {
+                    next = -next;
+                }
+                nextIdlePoint += next;
+            }
+        }
+    }
+    protected abstract bool ShouldUpdateNextPoint();
+    protected virtual void TransFromFollowPlayerToIdle()
+    {
+        Vector2 minpoint = transform.position;
+        float minDistance = float.MaxValue;
+        foreach (Transform point in idleRoute)
+        {
+            float thisDistance = Vector2.Distance(transform.position, point.position);
+            if (thisDistance < minDistance)
+            {
+                minpoint = point.position;
+                minDistance = thisDistance;
+            }
+        }
+        GoToNextPoint(minpoint);
+    }
 
+    protected abstract bool NeedCommand();
+    // protected abstract bool ShouldGotoNextIdlePoint();
+    protected abstract void GoToNextPoint(Vector2 nextPoint);
+    protected virtual void FoundPlayerBehavior()
+    {
+        isIdling = false;
+        if (rigid.velocity.y == 0)
+            GoToNextPoint(player.transform.position);
+    }
+
+    protected virtual void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
--- Assets/Scripts/AI/FrogAI.cs
+++ Assets/Scripts/AI/FrogAI.cs
@@ -2,53 +2,102 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FrogAI : MonoBehaviour {
-	public float maxSpeed = 3f;
-	public float jumpPower = 5f;
-	public float monitor_range = 10f;
-	public bool facingRight = true;
-	Rigidbody2D rigid;
-	Animator anim;
-	bool grounded = true;
-	GameObject player = null;
+public class FrogAI : BaseAI {
+	public float maxSpeed = 3f; //max x speed
 
-	// Use this for initialization
-	void Start () {
-		anim = GetComponent<Animator> ();
-		rigid = GetComponent<Rigidbody2D>();
-//		player = GameManager.Instance.GetManager<GameObjectManager> ().player;
-	}
-	
-	void FixedUpdate () {
-		if(player == null) player = GameManager.Instance.GetManager<GameObjectManager> ().player;
-		if (player == null)
-			return;
+	// public float jumpPower = 5f;
+	public float maxJumpDistance = 3f;
+    bool updateNextPoint=true;
+    protected override void GoToNextPoint(Vector2 nextPoint)
+    {
+        if (Vector2.Distance(nextPoint, transform.position) < maxJumpDistance)
+		{
+            JumpTowardPoint(nextPoint);
+            updateNextPoint = true;
+        }
+        else
+        {
+            // jump to the max distance point before the actual point
+            Vector2 targetPoint;
+            if (nextPoint.x > transform.position.x) // on the right
+                targetPoint = new Vector2(transform.position.x + maxJumpDistance, nextPoint.y);
+            else    // next point on the left
+                targetPoint = new Vector2(transform.position.x - maxJumpDistance, nextPoint.y);
+            JumpTowardPoint(targetPoint);
+            updateNextPoint = false;
+        }
+    }
 
-		if (rigid.velocity.y == 0 && !grounded)
-			grounded = true;
-			// anim.SetBool ("inair", false);
+	void JumpTowardPoint(Vector2 targetPoint)
+    {
+        float time;
+		Vector2 finalVelocity = CalculateJumpSpeed(targetPoint, out time);
+        if ((finalVelocity.x < 0 && facingRight) || (finalVelocity.x > 0 && !facingRight) ) Flip();
+        rigid.velocity = finalVelocity;
 
-		if (grounded) {
-			grounded = false;
-			// anim.SetBool ("inair", true);
-			anim.SetTrigger ("Jump1");
-			float move = 1;
-			if (Mathf.Abs (player.transform.position.x - transform.position.x) > monitor_range) {
-				move = (Random.Range (0, 2) == 0) ? -1 : 1;
-			} else {
-				move = (player.transform.position.x - transform.position.x > 0) ? 1 : -1;
-			}
+        anim.SetTrigger("Jump");
+        // anim.speed = 1/(time*2);
+        // Debug.Log(1/(time*2));
+    }
+ 
+    private Vector2 CalculateJumpSpeed(Vector2 targetPoint, out float time)
+    {	
+        float gravity = Physics.gravity.magnitude;
+        float xOffset = targetPoint.x - transform.position.x;
+        time = Mathf.Abs(xOffset/maxSpeed);
+        if (time == 0){
+            return new Vector2(0,0);
+        } 
 
-			if ((move > 0 && !facingRight) || (move < 0 && facingRight)) Flip();
-			rigid.velocity = new Vector2(move * maxSpeed, jumpPower);
-		}
-			
-	}
-	void Flip()
-	{
-		facingRight = !facingRight;
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+        float yOffset = targetPoint.y - transform.position.y;
+        float ySpeed = (yOffset + 0.5f * gravity * time * time) / time;
+        Vector2 finalVelocity;
+        if (xOffset > 0)
+        {
+            finalVelocity = new Vector2(maxSpeed, ySpeed);
+        }
+        else{
+            finalVelocity = new Vector2(-maxSpeed, ySpeed);
+        } 
+        // draw velocity line for debugging
+        // Debug.DrawLine(transform.position, (Vector3)finalVelocity + transform.position, Color.red, 2);
+        return finalVelocity;
+    }
+
+    protected override bool NeedCommand()
+    {
+        return rigid.velocity.y == 0;
+    }
+
+    protected override bool ShouldUpdateNextPoint()
+    {
+        return updateNextPoint;
+    }
+
+    // void FixedUpdate () {
+    // 	if(player == null) player = GameManager.Instance.GetManager<GameObjectManager> ().player;
+    // 	if (player == null)
+    // 		return;
+
+    // 	if (rigid.velocity.y == 0 && !grounded)
+    // 		grounded = true;
+    // 		// anim.SetBool ("inair", false);
+
+    // 	if (grounded) {
+    // 		grounded = false;
+    // 		// anim.SetBool ("inair", true);
+    // 		anim.SetTrigger ("Jump1");
+    // 		float move = 1;
+    // 		if (Mathf.Abs (player.transform.position.x - transform.position.x) > monitor_range) {
+    // 			move = (Random.Range (0, 2) == 0) ? -1 : 1;
+    // 		} else {
+    // 			move = (player.transform.position.x - transform.position.x > 0) ? 1 : -1;
+    // 		}
+
+    // 		if ((move > 0 && !facingRight) || (move < 0 && facingRight)) Flip();
+    // 		rigid.velocity = new Vector2(move * maxSpeed, jumpPower);
+    // 	}
+
+    // }
+
 }
