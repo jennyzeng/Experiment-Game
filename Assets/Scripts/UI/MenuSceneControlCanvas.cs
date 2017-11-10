f93939642c346ecf092c7e2f6a189969285e965d
jennyzeng <jennyzengzzh@gmail.com>
2017-11-09 18:16:38 -0800

    add about scene

---
+++ Assets/Scripts/UI/MenuSceneControlCanvas.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneControlCanvas : MonoBehaviour {
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("avatarTest");
+	}
+
+	public void About()
+	{
+		SceneManager.LoadScene("about");
+	}
+}
