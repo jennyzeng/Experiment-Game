9db00ba1dea45d31571e317e648940bd2ec41b4e
tingzhaz <tingzhaz@uci.edu>
2017-11-16 21:47:17 -0800

    Fix facing problem for Sonic and change some variables

--- Assets/Scripts/SonicAI/SonicAI.cs
+++ Assets/Scripts/SonicAI/SonicAI.cs
@@ -8,7 +8,6 @@ public class SonicAI : BaseAI
     public bool  moveRight = true;
     private float startPos;
     private float endPos;
-    //public bool isFacingRight;
 
     protected override void GoToNextPoint(Vector2 nextPoint)
     {
@@ -17,16 +16,15 @@ public class SonicAI : BaseAI
 
     void RunTowardPoint(Vector2 targetPoint)
     {
-        //float time;
         startPos = transform.position.x;
         endPos = targetPoint.x - transform.position.x;
-        //isFacingRight = transform.localScale.x > 0;
-        //Vector2 finalVelocity = CalculateRunSpeed(targetPoint, out time);
-        //if ((targetPoint.x < 0 && facingRight) || (targetPoint.x < 0 && !facingRight)) Flip();
+        if (rigid.position.x <= startPos)
+            moveRight = true;
+        
         if (moveRight)
         {
             rigid.AddForce(Vector2.right * EnemySpeed * Time.deltaTime);
-            if (targetPoint.x < 0 && facingRight)
+            if (facingRight)
                 Flip();
         }
 
@@ -35,16 +33,10 @@ public class SonicAI : BaseAI
 
         if (!moveRight)
         {
-            rigid.AddForce(-Vector2.right * EnemySpeed * Time.deltaTime * 10);
-            if (targetPoint.x < 0 && !facingRight)
+            rigid.AddForce(-Vector2.right * EnemySpeed * Time.deltaTime * 6);
+            if (!facingRight)
                 Flip();
         }
-        if (rigid.position.x <= startPos)
-            moveRight = true;
-
-        //rigid.velocity = finalVelocity;
-        //anim.speed = 1 / (time * 2);
-        //anim.SetTrigger("Run");
     }
 
    
