fab9a20489e717cef91b50264ffd2c6c8594a5f1
jennyzeng <jennyzengzzh@gmail.com>
2017-11-18 12:57:48 -0800

    create a spring, and a popup item for frog after it dies

---
+++ Assets/Scripts/Environment/Item/InteractiveItems/InterativeItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InterativeItem : MonoBehaviour {
+
+	/// <summary>
+	/// Sent when an incoming collider makes contact with this object's
+	/// collider (2D physics only).
+	/// </summary>
+	/// <param name="other">The Collision2D data associated with this collision.</param>
+	protected virtual void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			InteractAction(other);
+		}
+	}
+	protected abstract void InteractAction(Collision2D other);
+
+}
---
+++ Assets/Scripts/Environment/Item/InteractiveItems/Spring.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : InterativeItem {
+
+	public float magnitude = 10f;
+  /// <summary>
+  /// Start is called on the frame when a script is enabled just before
+  /// any of the Update methods is called the first time.
+  /// </summary>
+  Animator animator;
+  void Start()
+  {
+      animator = GetComponent<Animator>();
+  }
+    protected override void InteractAction(Collision2D other)
+    {
+      Vector2 offset = (Vector2)transform.position -other.contacts[0].point;
+      animator.SetTrigger("spring");
+
+      Rigidbody2D rigid = other.gameObject.GetComponent<Rigidbody2D>();
+      Vector2 force = new Vector2(rigid.velocity.x, magnitude);
+      rigid.velocity = force;
+    }
+
+}
