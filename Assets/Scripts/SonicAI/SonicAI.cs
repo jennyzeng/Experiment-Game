f128525bba9c93a9aed7e086f2bf30c6c079f6aa
tingzhaz <tingzhaz@uci.edu>
2017-11-18 16:54:28 -0800

    update Sonic movement(attack and back)

--- Assets/Scripts/SonicAI/SonicAI.cs
+++ Assets/Scripts/SonicAI/SonicAI.cs
@@ -8,6 +8,9 @@ public class SonicAI : BaseAI
     public bool  moveRight = true;
     private float startPos;
     private float endPos;
+    protected GameObjectManager playerPos;
+
+    //private float goBackPos;
 
     protected override void GoToNextPoint(Vector2 nextPoint)
     {
@@ -18,24 +21,27 @@ public class SonicAI : BaseAI
     {
         startPos = transform.position.x;
         endPos = targetPoint.x - transform.position.x;
-        if (rigid.position.x <= startPos)
-            moveRight = true;
+        //goBackPos = playerPos.playerTransform.position.x;
+        //playerPos = player.transform.position.x;
+        if (rigid.position.x >= startPos)
+            moveRight = true;  
+        if (rigid.position.x >= endPos)
+        {
+            moveRight = false;
+        }
         
+
         if (moveRight)
         {
+            if (facingRight) Flip();
             rigid.AddForce(Vector2.right * EnemySpeed * Time.deltaTime);
-            if (facingRight)
-                Flip();
+            //DetectPlayer();
         }
-
-        if (rigid.position.x >= endPos)
-            moveRight = false;
-
         if (!moveRight)
         {
-            rigid.AddForce(-Vector2.right * EnemySpeed * Time.deltaTime * 6);
-            if (!facingRight)
-                Flip();
+            rigid.AddForce(-Vector2.right * EnemySpeed * Time.deltaTime * 10);
+            if (!facingRight) Flip();
+               
         }
     }
 
