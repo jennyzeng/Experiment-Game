895d24b31a050616f281c259a54e0922e16fa07d
tingzhaz <tingzhaz@uci.edu>
2017-12-07 21:08:57 -0800

    update Sonic AI pop up item & add Sonic Ai to level2 map

---
+++ Assets/Scripts/Environment/Item/MonsterItems/SonicRunItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Ball movement ability
+public class SonicRunItem : MonsterItem<PlayerBallMovement> {
+    protected override PlayerBallMovement OnAddComponent(Collider2D player)
+    {
+        player.transform.parent.GetComponent<BodySwitcher>().canSwitchToBall= true;
+        Destroy(gameObject);
+        return null;
+    }
+}
